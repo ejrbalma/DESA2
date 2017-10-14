d281b3930eda0817b71defc184d3f88e9938b7fd
Enrique José Ramón Balmaseda <ejrbalma@gmail.com>
2017-10-14 19:01:00 +0100

    ADD to version MVC

---
+++ src/histogrambranch/CalculaHistogram.java
@@ -0,0 +1,13 @@
+package histogrambranch;
+
+public class CalculaHistogram {
+      
+    public static <T> Histogram <T> computeHistogram (T[] vector){
+        
+        Histogram <T> histo = new Histogram<> ();
+        for (T key : vector) {
+            histo.increment(key);
+        }
+        return histo;
+    }    
+}
